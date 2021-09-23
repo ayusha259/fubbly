c88975a687e18f07c8c1e4fa3d1174b475399630
Ayush <ayusha259@gmail.com>
2021-09-23 15:55:36 +0530

    "Ready"

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./utils/connectDb.js";
 import { notFound, errHandler } from "./middlewares/errHandlers.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -14,6 +19,13 @@ const app = express();
 
 app.use(express.json());
 app.use("/uploads", express.static("./backend/uploads"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.use("/api/users/", userRoutes);
 app.use("/api/posts/", postRoutes);
