900d86c38c9a8c245014e09956b52fc20fff9353
Ayush <ayusha259@gmail.com>
2021-10-01 18:45:08 +0530

    not found error

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/uploads", express.static("./backend/uploads"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./frontend/build"));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build"));
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
 }
 
