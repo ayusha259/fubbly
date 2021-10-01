9e2b5062454f08db8366c3b1deb201191cf3305b
Ayush <ayusha259@gmail.com>
2021-10-01 18:53:11 +0530

    "server update"

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,8 @@ const app = express();
 
 app.use(express.json());
 app.use("/uploads", express.static("./backend/uploads"));
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./frontend/build"));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
